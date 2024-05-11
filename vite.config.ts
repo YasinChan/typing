da7d0ee044c4a6399eda72836575db11edff351a
YasinChan <yasinchan2016@gmail.com>
2024-05-11 10:09:46 +0800

    feat: 增加 gzip 压缩

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import svgLoader from 'vite-svg-loader';
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), svgLoader()],
+  plugins: [vue(), vueJsx(), svgLoader(), viteCompression()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
