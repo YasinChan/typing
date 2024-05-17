65ba2bc735a038941db847a5d34ab8e97a79cd65
Yasinchan <yasinchan2016@gmail.com>
2024-05-17 03:34:29 +0000

    Build and deploy

--- src/common/reg.ts
+++
@@ -1,3 +0,0 @@
-export const USERNAME_REG = /^[\u4e00-\u9fffa-zA-Z0-9_-]{1,25}$/;
-export const PASSWORD_REG = /^[a-zA-Z0-9]{6,}$/;
-export const EMAIL_REG = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
