3e7b14ea6dbd8338fcffb98d54aed699087251b0
YasinChan <yasinchan2016@gmail.com>
2025-03-11 13:01:15 +0800

    feat: 提供传统浏览器兼容性支持。

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,22 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import svgLoader from 'vite-svg-loader';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), svgLoader()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    svgLoader(),
+    legacy({
+      targets: ['defaults', 'not IE 11'], // 指定兼容的浏览器范围
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'] // 如果需要，添加额外的 polyfills
+    })
+  ],
+  build: {
+    target: 'es2015' // 指定构建目标为 ES2015，以兼容更多浏览器
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
